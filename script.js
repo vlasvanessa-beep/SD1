14de9626b2fb25594995da78c9f9d1efb096a9bb
vlasvanessa-beep <vlas.vanessa@gmail.com>
2026-04-09 19:33:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Inicializa o mapa
-const map = L.map('map').setView([-23.561684, -46.655981], 15);
+const map = L.map('map').setView([-23.6872491414978, -46.553723770379456], 15);
 
 // Adiciona o tile layer do OpenStreetMap
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,7 +7,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Adiciona um marcador na localização da loja
-L.marker([-23.561684, -46.655981])
+L.marker([-23.6872491414978, -46.553723770379456])
   .addTo(map)
-  .bindPopup("Loja Exemplo<br>Rua das Flores, 123 - São Paulo/SP")
+  .bindPopup("Faculdade Anhanguera SBC<br>R. Atlântica, 731 - Jardim do Mar, São Bernardo do Campo - SP, 09750-480")
   .openPopup();
